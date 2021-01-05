da1ce02c3369c207b61778abbd83aa09f764f5c9
zuoziye1 <478251468@qq.com>
2021-01-05 23:36:55 +0800

    do

---
+++ src/main/java/com/leecodestudy/leecode/数组相关/二分查找.java
@@ -0,0 +1,31 @@
+package com.leecodestudy.leecode.数组相关;
+
+public class 二分查找 {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1,2,3,4,5,6};
+        int index = getIndex(array, 1);
+        System.out.println(index);
+    }
+
+    public static int getIndex(int[] array,int target){
+        int start = 0 ;
+        int end = array.length -1 ;
+        while (start <= end ){
+            int midIndex = (start+end)/2;
+            int mid = array[midIndex] ;
+
+            if(mid == target){
+                return midIndex ;
+            }
+
+            if(mid < target){
+                start = midIndex + 1 ;
+            }
+            if(mid > target){
+                end = midIndex - 1 ;
+            }
+        }
+        return -1 ;
+    }
+}
--- src/main/java/com/leecodestudy/leecode/数组相关/快速排序.java
+++ src/main/java/com/leecodestudy/leecode/数组相关/快速排序.java
@@ -30,7 +30,16 @@ public class 快速排序 {
         while (start < end) {
 
             /**
-             * 右侧先动，能让他停下的位置，一定是小于等于基准值，或者是与左侧的index碰头了
+             *
+             * https://www.processon.com/diagraming/5e0490ece4b0aef94cb0b11f
+             *
+             * 右侧先动，能让他停下的位置，一定是小于等于基准值，或者是与左侧的index碰头了，最差的情况二者在 index = 0 处 碰头
+             *
+             * 如果左侧先动了，就可能会出现在index = length -1 处碰头了，而基准值又是 index = 0
+             * 碰头后又需要交换二者，这就出现了基准值左边出现了大于基准值的数据，就错了。
+             *
+             * 为什么临界点要有等号，因为我们要把数组按照基准值分成两半，左边都是小于等于它的，右边都是大于等于它的，
+             * 为什么需要 start < end ，因为当end--后，start未动，但是可能也会出现end = start
              */
             while (array[end] >= standard && start < end) {
                 end--;
